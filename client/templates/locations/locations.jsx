bc3925331d04689ba93b1f654b1449c8edf9a3c9
Janne Parkkila <japskua@gmail.com>
2015-05-29 18:53:38 -0400

    Changed the location cards to use react

---
+++ client/templates/locations/locations.jsx
@@ -0,0 +1,65 @@
+/**
+ * This directive is necessary to enable preprocessing of JSX tags:
+ * @jsx React.DOM
+ */
+
+var cx = React.addons.classSet;
+
+var Location = ReactMeteor.createClass({
+    render : function() {
+        return (
+            <div className="location card col s4">
+                <div class="card-image">
+                    <p>Image here</p>
+                    <span class="card-title">{this.props.name}</span>
+                </div>
+                <div class="location-content card-content">
+                    <p>{this.props.coords}</p>
+                    <p>{this.props.description}</p>
+                    <p>Added by {this.props.author} on {this.props.submitted}</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Link</a>
+                </div>
+            </div>
+        );
+    }
+});
+
+var LocationsList = ReactMeteor.createClass({
+    templateName : 'LocationsList',
+    startMeteorSubscriptions : function() {
+        Meteor.subscribe('locations');
+    },
+    getMeteorState : function() {
+        return {
+            data : Locations.find({}).fetch()
+        };
+    },
+    getInitialState : function() {
+        return { data : [] }
+    },
+    render : function() {
+        var locationNodes = this.state.data.map(function(location) {
+            return (
+                <Location key={location._id} name={location.name}
+                          coords={location.coords} description={location.description}
+                          author={location.author} submitted={location.submitted}>
+                </Location>
+            );
+        });
+        return (
+            <div className="locationsList">
+                {locationNodes}
+            </div>
+        );
+    }
+});
+
+LocationsListRender = function() {
+    React.render(
+        <LocationsList />,
+        document.getElementById('container')
+    );
+};
+
---
+++ client/templates/locations/locations_list.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Janne on 29.5.2015.
+ */
+Template.LocationsList.helpers({
+    onRendered : function() {
+        LocationsListRender();
+    }
+});
